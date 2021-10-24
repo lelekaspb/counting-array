d88717a8e477989a678a0c4942ec4f0de0f090ac
lelekaspb <bov-1986@yandex.ru>
2021-10-24 13:23:17 +0200

    changed README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-https://lelekaspb.github.io/counting-array/
+![Screenshot](array_counter.gif)
+
+Every second, the counter increments by one. The counter is then added to the beginning of an array. The array is “truncated” so that it never becomes longer than 9.
+
+The array is logged to the console.
--- counting_array.js
+++ counting_array.js
@@ -1,19 +1,16 @@
 "use strict";
 
-console.log("HELLO");
-
 const myArray = [];
 let counter = 0;
 
 function countloop() {
-    // console.log(counter);
-    myArray.unshift(counter);
-    console.log(myArray);
-    counter++;
-    if (myArray.length > 8) {
-        myArray.length = 8;
-    }
-    setTimeout(countloop, 1000);
+  myArray.unshift(counter);
+  console.log(myArray);
+  counter++;
+  if (myArray.length > 8) {
+    myArray.length = 8;
+  }
+  setTimeout(countloop, 1000);
 }
 
 countloop();
